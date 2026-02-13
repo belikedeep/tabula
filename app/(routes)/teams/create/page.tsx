b4f9bdc34a32af3ca5d468fb0f9e9e4189cccc0d
Deepak Mardi <deepakmardi22@gmail.com>
2026-02-13 23:52:00 +0530

    feat: Implement team creation functionality in CreateTeam component and add Toaster for notifications

--- app/(routes)/teams/create/page.tsx
+++ app/(routes)/teams/create/page.tsx
@@ -1,10 +1,28 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { api } from "@/convex/_generated/api";
+import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
+import { useMutation } from "convex/react";
 import { useState } from "react";
+import { toast } from "sonner";
 
 const CreateTeam = () => {
   const [teamName, setTeamName] = useState("");
+  const createTeam = useMutation(api.teams.createTeam);
+  const { user } = useKindeBrowserClient();
+
+  const createNewTeam = () => {
+    createTeam({
+      teamName: teamName,
+      createdBy: user?.email!,
+    }).then((res) => {
+      console.log(res);
+      if (res) {
+        toast("Teams created successfully");
+      }
+    });
+  };
 
   return (
     <div>
