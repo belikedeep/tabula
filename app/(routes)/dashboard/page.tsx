985bd4b50e4e3220519d5297d56127c0c8dd8206
Deepak Mardi <deepakmardi22@gmail.com>
2026-02-13 17:20:18 +0530

    feat: Enhance Dashboard component with Logout button using Kinde authentication

--- app/(routes)/dashboard/page.tsx
+++ app/(routes)/dashboard/page.tsx
@@ -1,5 +1,15 @@
+import { Button } from "@/components/ui/button";
+import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs";
+
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div>
+      Dashboard
+      <Button>
+        <LogoutLink>Logout</LogoutLink>
+      </Button>
+    </div>
+  );
 };
 
 export default Dashboard;
