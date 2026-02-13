02142e4a0e6f9342c8f88b4cfc8ba227d885260f
Deepak Mardi <deepakmardi22@gmail.com>
2026-02-13 23:40:09 +0530

    feat: Implement CreateTeam component and add createTeam mutation for team creation

--- app/(routes)/teams/create/page.tsx
+++ app/(routes)/teams/create/page.tsx
@@ -1,5 +1,25 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+
 const CreateTeam = () => {
-  return <div>CreateTeam</div>;
+  const [teamName, setTeamName] = useState("");
+
+  return (
+    <div>
+      <div className="flex justify-center pt-20">
+        <h2>What should we call your team</h2>
+      </div>
+      <div className="flex flex-col gap-2 justify-center pt-10">
+        <Input
+          onChange={(e) => setTeamName(e.target.value)}
+          placeholder="Team Name"
+        />
+        <Button disabled={!teamName && teamName?.length > 0}>Create</Button>
+      </div>
+    </div>
+  );
 };
 
 export default CreateTeam;
