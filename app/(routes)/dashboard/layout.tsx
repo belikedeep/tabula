307bde9db246da501a7fbb1779b116c4851671c4
Deepak Mardi <deepakmardi22@gmail.com>
2026-02-13 23:21:57 +0530

    fix: Update team check logic in DashboardLayout to trigger on user change

--- app/(routes)/dashboard/layout.tsx
+++ app/(routes)/dashboard/layout.tsx
@@ -1,6 +1,8 @@
+"use client";
 import { api } from "@/convex/_generated/api";
 import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
 import { useConvex } from "convex/react";
+import { useRouter } from "next/navigation";
 import React, { useEffect } from "react";
 
 const DashboardLayout = ({
@@ -10,17 +12,20 @@ const DashboardLayout = ({
 }>) => {
   const convex = useConvex();
   const { user } = useKindeBrowserClient();
+  const router = useRouter();
 
   useEffect(() => {
-    checkTeam();
-  }, []);
+    user && checkTeam();
+  }, [user]);
 
   const checkTeam = async () => {
     const result = await convex.query(api.teams.getTeam, {
       email: user?.email!,
     });
 
-    return result;
+    if (!result.length) {
+      router.push("/teams/create");
+    }
   };
   return <div>{children}</div>;
 };
