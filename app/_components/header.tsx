e336ac9e57ec31556c1a63eb8cc087aa77537b6e
Deepak Mardi <deepakmardi22@gmail.com>
2026-02-13 17:01:26 +0530

    feat: Add Shadcn UI components, utility functions, and establish initial page layout with header and hero sections.

---
+++ app/_components/header.tsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <div className="flex gap-10 justify-center">
+      <div>Login</div>
+      <div>Register</div>
+    </div>
+  );
+};
+
+export default Header;
