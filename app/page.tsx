dc607889f70cd66e22810768cbce309fc42b8bad
Deepak Mardi <deepakmardi22@gmail.com>
2026-02-13 17:17:57 +0530

    feat: Integrate Kinde authentication client and log user data on home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
 "use client";
 
+import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
 import Header from "./_components/header";
 import Hero from "./_components/hero";
+import { useEffect } from "react";
 
 const Home = () => {
+  const { user } = useKindeBrowserClient();
+
+  useEffect(() => {
+    console.log("--", user);
+  }, [user]);
   return (
     <div>
       <Header />
