35ec4f8f1d83d6001ad82b314ceff090ecab6f73
DEEPAK MARDI <deepakmardi22@gmail.com>
2026-02-16 09:13:39 +0530

    feat: Implement dashboard header with search input, user profile image, and invite button using Kinde user data.

--- app/(routes)/dashboard/_components/header.tsx
+++ app/(routes)/dashboard/_components/header.tsx
@@ -1,6 +1,21 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input"
+import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
+import Image from "next/image"
 
 export const Header = () => {
+    const { user } = useKindeBrowserClient();
     return (
-        <div>Header</div>
+        <div className="flex justify-end w-full p-10">
+            <div>
+                <Input placeholder="Search" type="text" />
+            </div>
+            <div>
+                <Image src={user?.picture || ""} alt="user image" width={30} height={30} className="rounded-full" />
+            </div>
+            <div>
+                <Button >Invite</Button>
+            </div>
+        </div>
     )
 }
