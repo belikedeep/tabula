f5ddc43f67f402ba57181e8bf3b0e6b15c4568b5
DEEPAK MARDI <deepakmardi22@gmail.com>
2026-02-16 19:15:51 +0530

    feat: configure Excalidraw with custom UI options, main menu, and welcome screen.

--- app/(routes)/workspace/_components/canvas.tsx
+++ app/(routes)/workspace/_components/canvas.tsx
@@ -1,5 +1,6 @@
 import dynamic from "next/dynamic";
 import "@excalidraw/excalidraw/index.css";
+import { MainMenu, WelcomeScreen } from "@excalidraw/excalidraw";
 
 const Excalidraw = dynamic(
   async () => (await import("@excalidraw/excalidraw")).Excalidraw,
@@ -11,7 +12,37 @@ const Excalidraw = dynamic(
 const Canvas = () => {
   return (
     <div className="h-full w-full">
-      <Excalidraw />
+      <Excalidraw
+        theme="light"
+        onChange={(excalidrawElements, appState, files) => {
+          console.log(excalidrawElements);
+          console.log(appState);
+          console.log(files);
+        }}
+        UIOptions={{
+          canvasActions: {
+            saveToActiveFile: false,
+            loadScene: false,
+            export: false,
+            toggleTheme: false,
+          },
+        }}
+      >
+        <MainMenu>
+          <MainMenu.DefaultItems.ClearCanvas />
+          <MainMenu.DefaultItems.SaveAsImage />
+          <MainMenu.DefaultItems.ChangeCanvasBackground />
+        </MainMenu>
+        <WelcomeScreen>
+          <WelcomeScreen.Hints.MenuHint />
+          <WelcomeScreen.Hints.ToolbarHint />
+          <WelcomeScreen.Center>
+            <WelcomeScreen.Center.Heading>
+              Welcome to Tabula
+            </WelcomeScreen.Center.Heading>
+          </WelcomeScreen.Center>
+        </WelcomeScreen>
+      </Excalidraw>
     </div>
   );
 };
