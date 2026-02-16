bc266ad0970491ad468fd7068bfe6066f352a927
DEEPAK MARDI <deepakmardi22@gmail.com>
2026-02-16 20:34:06 +0530

    feat: Introduce a free file limit, display a pricing dialog when exceeded, and define constants for file limits.

--- app/(routes)/dashboard/_components/sideNavBottomSection.tsx
+++ app/(routes)/dashboard/_components/sideNavBottomSection.tsx
@@ -12,6 +12,9 @@ import {
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { useState } from "react";
+import constants from "@/app/_constants/Constants";
+import Constants from "@/app/_constants/Constants";
+import PricingDialog from "./pricingDialog";
 
 const SideNavBottomSection = ({ onFileCreate, totalFiles }: any) => {
   const [fileInput, setFileInput] = useState("");
@@ -50,40 +53,46 @@ const SideNavBottomSection = ({ onFileCreate, totalFiles }: any) => {
         <DialogTrigger asChild>
           <Button className="w-full">New File</Button>
         </DialogTrigger>
-        <DialogContent>
-          <DialogHeader>
-            <DialogTitle>Create a New file</DialogTitle>
-            <DialogDescription>
-              <Input
-                onChange={(e) => setFileInput(e.target.value)}
-                placeholder="Enter file name"
-              />
-            </DialogDescription>
-          </DialogHeader>
-          <DialogFooter>
-            <DialogClose asChild>
-              {/* TODO can add lenth condition as well */}
-              <Button
-                disabled={!fileInput}
-                type="button"
-                onClick={() => onFileCreate(fileInput)}
-              >
-                Create
-              </Button>
-            </DialogClose>
-          </DialogFooter>
-        </DialogContent>
+        {totalFiles < Constants.MAX_FREE_FILES ? (
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Create a New file</DialogTitle>
+              <DialogDescription>
+                <Input
+                  onChange={(e) => setFileInput(e.target.value)}
+                  placeholder="Enter file name"
+                />
+              </DialogDescription>
+            </DialogHeader>
+            <DialogFooter>
+              <DialogClose asChild>
+                {/* TODO can add lenth condition as well */}
+                <Button
+                  disabled={!fileInput}
+                  type="button"
+                  onClick={() => onFileCreate(fileInput)}
+                >
+                  Create
+                </Button>
+              </DialogClose>
+            </DialogFooter>
+          </DialogContent>
+        ) : (
+          <PricingDialog />
+        )}
       </Dialog>
 
       {/* Progress Bar */}
       <div className="h-4 w-full bg-gray-200 rounded-full mt-5">
         <div
           className="h-4 bg-primary rounded-full"
-          style={{ width: `${(totalFiles / 5) * 100}%` }}
+          style={{ width: `${(totalFiles / Constants.MAX_FREE_FILES) * 100}%` }}
         ></div>
       </div>
 
-      <h2>{totalFiles} out of 5 files used</h2>
+      <h2>
+        {totalFiles} out of {Constants.MAX_FREE_FILES} files used
+      </h2>
       <h2>Upgrade your plan for unlimited access </h2>
     </div>
   );
