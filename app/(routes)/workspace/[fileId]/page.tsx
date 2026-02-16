cc6e303f0ee8e130aead4e6c45bf11ac7a705f27
DEEPAK MARDI <deepakmardi22@gmail.com>
2026-02-16 11:17:49 +0530

    feat: Implement a two-column grid layout for document and canvas sections on the workspace page.

--- app/(routes)/workspace/[fileId]/page.tsx
+++ app/(routes)/workspace/[fileId]/page.tsx
@@ -4,6 +4,14 @@ const WorkspacePage = () => {
   return (
     <div>
       <WorkspaceHeader />
+
+      {/* layout */}
+      <div className="grid grid-cols-1 md:grid-cols-2">
+        {/* document */}
+        <div className="bg-blue-400 h-screen">Document</div>
+        {/* canvas */}
+        <div className="bg-red-400 h-screen">Canvas</div>
+      </div>
     </div>
   );
 };
